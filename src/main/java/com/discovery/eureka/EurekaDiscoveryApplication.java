ce7b98741b38cb567ca1a4444c6a6581294aa9b4
unknown <ankit1990.pathak@gmail.com>
2022-03-22 12:37:39 +0530

    code adding

---
+++ src/main/java/com/discovery/eureka/EurekaDiscoveryApplication.java
@@ -0,0 +1,13 @@
+package com.discovery.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaDiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaDiscoveryApplication.class, args);
+	}
+
+}
